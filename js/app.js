77414093fac73d3de1c7f13d5ebf07a9f2ada9e1
sahhebali <saheb.co21@gmail.com>
2021-09-19 12:14:13 +0530

    module27/-7

--- js/app.js
+++ js/app.js
@@ -17,3 +17,35 @@ function generatePin() {
     const pin = getPin();
     document.getElementById('display-pin').value = pin;
 }
+
+document.getElementById('key-pad').addEventListener('click', function () {
+    const numbers = event.target.innerText;
+    const clacInput = document.getElementById('typed-numbers');
+    if (isNaN(numbers)) {
+        if (numbers == 'C') {
+            clacInput.value = '';
+        }
+    }
+    else {
+        const previousNumber = clacInput.value;
+        const newNumber = previousNumber + numbers;
+        clacInput.value = newNumber;
+    }
+
+})
+// submit button
+function verifyPin() {
+    const pin = document.getElementById('display-pin').value;
+    const typedNumbers = document.getElementById('typed-numbers').value;
+    const successmassege = document.getElementById('notify-success');
+    const notifyeror = document.getElementById('notify-fail');
+    if (pin == typedNumbers) {
+        successmassege.style.display = 'block';
+        notifyeror.style.display = 'none';
+    }
+    else {
+
+        notifyeror.style.display = 'block';
+        successmassege.style.display = 'none';
+    }
+}
